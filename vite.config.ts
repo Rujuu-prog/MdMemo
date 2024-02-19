fcfa8552a6e4abd9758a76e2dab84c31796f940b
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2024-02-20 00:17:32 +0900

    :bag: fixed a bug that prevented build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 export default defineConfig({
+  base: "./",
   plugins: [
     vue(),
     Components({
